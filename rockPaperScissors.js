b1477d4ca0cfec21d09db5a56846ffee175508dd
JoeFalzone <falzonej@gmail.com>
2022-01-06 00:54:39 -0500

    added scoring variables, and attempted to create encasing func for replability without having to refresh browser, likely witnessing scope issues currently -- added comments for clarity on func purposes

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,5 +1,16 @@
-computerOptions = ['rock', 'paper', 'scissor'];
+//stores player and computer scores
+let computerScore = 0
+let playerScore = 0
+const computerOptions = ['rock', 'paper', 'scissor'];
 
+function playRound(){ 
+    computerPick()
+    playerChoiceOrg()
+    theBattle()
+
+//computer's options, random number selector, and stored choice for comparison 
+const computerOptions = ['rock', 'paper', 'scissor'];
+let computerChoice = computerPick();
 function computerNumber() {
     result = Math.floor(Math.random()*3)
     return result;
@@ -8,39 +19,48 @@ function computerPick() {
     result = computerOptions[computerNumber()]
     return result;
 }
-
-let computerChoice = computerPick();
-
+// user choice and lowercasing to allow comparison
 let playerChoiceOrg = window.prompt("Chose your weapon: Rock, Paper, Scissors");
 let playerChoice = playerChoiceOrg.toLowerCase();
 
+//computer and player choice comparison and score incrementation 
 function theBattle() {
 
+    //if (playerChoice == null)
+    //return console.log('Indecision is no way to win The Battle')
+
     if (playerChoice === computerChoice) {
-        return console.log("Tie!")
+        return console.log("Tie!");
     }
     else if (playerChoice == 'rock'){ 
         if (computerChoice == 'paper'){
-            return console.log('Computer Won!')
+            computerScore++;
+            return console.log('Computer Won, Paper beats Rock!');
         }  
-    else{
-            return console.log('Player Won')
+        else{
+            playerScore++
+            return console.log('Player Won, Rock beats Scissors!');
         }
     }
     else if (playerChoice == 'scissors'){
         if(computerChoice == 'rock'){
-            return console.log('Computer Won!')
+            computerScore++;
+            return console.log('Computer Won, Rock beats Scissors!');
         }
         else{
-            return console.log('Player Won')
+            playerScore++
+            return console.log('Player Won, Scissors beat Paper!');
         }
     }
     else if (playerChoice == 'paper'){
-        if (computerChoice == 'rock'){
-            return console.log('Computer Won')
+        if (computerChoice == 'scissors'){
+            computerScore++;
+            return console.log('Computer Won, Scissors beat Paper!');
         }
         else{
-            return console.log('Player Won')
+            playerScore++
+            return console.log('Player Won, Paper beats Rock!');
         }
     }
+}
 }
