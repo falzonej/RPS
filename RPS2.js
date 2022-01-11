561a9ba7a6f3a487b84c2f7876f7dfdcd83c4e2b
JoeFalzone <falzonej@gmail.com>
2022-01-11 00:45:39 -0500

    adjusted punctuation and indentation

--- RPS2.js
+++ RPS2.js
@@ -2,7 +2,7 @@
 let computerScore = 0;
 let playerScore = 0;
 
-//full game 
+//runs full 5 round game and returns the winner 
 function playGame() {
     while (playerScore || computerScore < 4) 
     if (playerScore > 4)
@@ -34,30 +34,27 @@ function playRound() {
 
     //comparison function to check playerChoice against computerChoice and return strings based on findings
     function theBattle() {  
-
         if (playerChoice === computerChoice) {
-            return console.log('Tie!')
+            return console.log('Tie!'), console.log(playerChoice), console.log(computerChoice)
             }else {
-                console.log(playerChoice) 
-                console.log(computerChoice)
-            if (playerChoice == 'rock' && computerChoice == 'scissors') {
-                    let playerWinnerRock = 'Rock beats Scissors'
-                    playerScore++;
-                    return console.log(playerWinnerRock);
-                }else if (playerChoice == 'scissors' && computerChoice == 'paper') {
-                    let playerWinnerScissors = 'Scissors beats Paper!'
-                    playerScore++;
-                    return console.log(playerWinnerScissors);
-                }else if (playerChoice == 'paper' && computerChoice == 'rock') {
-                    let playerWinnerPaper = 'Paper beats Rock!'
-                    playerScore++;
-                    return console.log(playerWinnerPaper);}
-            else {
-                let cpuWinner = "Computer Wins" 
-                computerScore++;
-                return console.log(cpuWinner);}
-            };
-        }
+                if (playerChoice == 'rock' && computerChoice == 'scissors') {
+                        let playerWinnerRock = 'Rock beats Scissors!'
+                        playerScore++;
+                        return console.log(playerWinnerRock);
+                    }else if (playerChoice == 'scissors' && computerChoice == 'paper') {
+                        let playerWinnerScissors = 'Scissors beats Paper!'
+                        playerScore++;
+                        return console.log(playerWinnerScissors);
+                    }else if (playerChoice == 'paper' && computerChoice == 'rock') {
+                        let playerWinnerPaper = 'Paper beats Rock!'
+                        playerScore++;
+                        return console.log(playerWinnerPaper);}
+                else {
+                    let computerWinner = "Computer Wins!" 
+                    computerScore++;
+                    return console.log(computerWinner);}
+                }
+            }
 
 return console.log(theBattle());
 }
